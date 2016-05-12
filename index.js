5eb8003658a774bcb84301a0e310378fba3bd947
Jason Tham <jason.tham@tempus.com>
2016-05-12 14:42:34 -0500

    created arraysEqual and objectsEqual

---
+++ index.js
@@ -0,0 +1,27 @@
+// Function takes two arrays and returns
+module exports function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+
+    return true;
+}
+
+module exports function objectsEqual(o1,o2){
+  var keys1 = Object.keys(o1);
+  var keys2 = Object.keys(o2);
+  if(!arraysEqual(keys1,keys2)) {
+    console.log("keys not equal");
+    return false
+  }
+  for(var i = 0; i < keys1.length; i++){
+    if(o1[keys1[i]] != o2[keys1[i]]){
+      console.log("value of " + keys1[i] + " not equal");
+      return false;
+    }
+  }
+  return true
+}
