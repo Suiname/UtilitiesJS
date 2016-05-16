e24b9f555c4a014958b668349bf3ae5fb878549e
Jason Tham <jason.tham@tempus.com>
2016-05-16 15:36:25 -0500

    fixed bug where keys are different returning true

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-function objectsEqual(o1, o2){
-  var keys1 = Object.keys(o1);
-  var keys2 = Object.keys(o2);
-  if(keys1.length !== keys2.length){
-    return false;
-  }
-  for(var i = keys1.length; i--;) {
-    if (typeof o1[keys1[i]] !== 'object') {
-      if(o1[keys1[i]] !== o2[keys2[i]]){
-        return false;
-      }
-    } else {
-      if(!objectsEqual(o1[keys1[i]],o2[keys2[i]])){
-        return false;
+function objectsEqual(o1, o2) {
+	if (typeof o1 !== typeof o2)
+		return false
+
+	if (typeof o1 === 'object') {
+		var k1 = Object.keys(o1);
+		var k2 = Object.keys(o2);
+		if (k1.length !== k2.length)
+			return false
+    for (var i = 0; i < k1.length; i++) {
+      if (k1[i] !== k2[i]) {
+        return false
       }
     }
-  }
-  return true;
+		for (var i = 0; i < k1.length; i++) {
+			if (! objectsEqual(o1[k1[i]], o2[k2[i]]))
+      	return false
+		}
+    return true
+	}
+	else
+		return o1 === o2
 }
+
 module.exports = {objectsEqual};
--- test/index_spec.js
+++ test/index_spec.js
@@ -8,10 +8,11 @@ var assert = require('chai').assert,
 describe('#objectsEqual', function(){
   var obj1 = {"name": "Jason", "job": "coding"},
     obj2 = {"name": "Jason", "job": "coding"},
-    obj3 = {"name": "Jason", "job": "not coding"};
-    obj4 = {"first": obj1, "second": obj2, "third": { first: obj1, second: obj2}, fourth: [1,2,3]};
-    obj5 = {"first": obj1, "second": obj2, "third": { first: obj1, second: obj2}, fourth: [1,2,3]};
-    obj6 = {"first": obj2, "second": obj1, "third": { first: obj4, second: obj2}, fourth: [1,3]};
+    obj3 = {"name": "Jason", "job": "not coding"},
+    obj4 = {"first": obj1, "second": obj2, "third": { first: obj1, second: obj2}, fourth: [1,2,3]},
+    obj5 = {"first": obj1, "second": obj2, "third": { first: obj1, second: obj2}, fourth: [1,2,3]},
+    obj6 = {"first": obj2, "second": obj1, "third": { first: obj4, second: obj2}, fourth: [1,3]},
+    obj7 = {"what": "Jason", "job": "coding"};
 
 
   it('takes two objects and returns true or false', function(){
@@ -29,4 +30,7 @@ describe('#objectsEqual', function(){
   it('takes two different objects with nested key value pairs and returns false', function(){
     assert.isFalse(objectsEqual(obj5,obj6), "This will be false")
   });
+  it('takes two different objects with the same values but different keys and returns false', function(){
+    assert.isFalse(objectsEqual(obj2, obj7), "This will be false")
+  })
 })
