8a47e0f6e38cd8d55b79f979bb1d2b148bbe5b15
Jason Tham <jason.tham@tempus.com>
2016-05-26 17:28:42 -0500

    added more sorts, tests, updated readme

--- sorting.js
+++ sorting.js
@@ -32,4 +32,65 @@ function selectionSort(arr){
   return arr;
 }
 
-module.exports = {bubbleSort, selectionSort};
+
+function insertionSort(arr) {
+
+    var len     = arr.length,     // number of items in the array
+        value,                      // the value currently being compared
+        i,                          // index into unsorted section
+        j;                          // index into sorted section
+
+    for (i=0; i < len; i++) {
+
+        // store the current value because it may shift later
+        value = arr[i];
+
+        /*
+         * Whenever the value in the sorted section is greater than the value
+         * in the unsorted section, shift all items in the sorted section over
+         * by one. This creates space in which to insert the value.
+         */
+        for (j=i-1; j > -1 && arr[j] > value; j--) {
+            arr[j+1] = arr[j];
+        }
+
+        arr[j+1] = value;
+    }
+
+    return arr;
+}
+
+function mergeSort(arr){
+
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    var middle = Math.floor(arr.length / 2),
+        left    = arr.slice(0, middle),
+        right   = arr.slice(middle),
+        params = merge(mergeSort(left), mergeSort(right));
+
+    // Add the arguments to replace everything between 0 and last item in the array
+    params.unshift(0, arr.length);
+    arr.splice.apply(arr, params);
+    return arr;
+}
+
+function merge(left, right){
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    while (il < left.length && ir < right.length){
+        if (left[il] < right[ir]){
+            result.push(left[il++]);
+        } else {
+            result.push(right[ir++]);
+        }
+    }
+
+    return result.concat(left.slice(il)).concat(right.slice(ir));
+}
+
+module.exports = {bubbleSort, selectionSort, insertionSort, mergeSort};
--- test/index_spec.js
+++ test/index_spec.js
@@ -6,6 +6,8 @@ var assert = require('chai').assert,
   randomInt = utils.randomInt;
   bubbleSort = utils.sort.bubbleSort;
   selectionSort = utils.sort.selectionSort;
+  insertionSort = utils.sort.insertionSort;
+  mergeSort = utils.sort.mergeSort;
 
 
 
@@ -66,6 +68,7 @@ describe('#bubbleSort', function(){
   var notSorted = [1,9,4,5,6,2,10,3,8,7];
   var sorted = [1,2,3,4,5,6,7,8,9,10];
   it('takes an unsorted array and sorts it', function(){
+    assert.notDeepEqual(sorted,notSorted, "This should return false")
     bubbleSort(notSorted);
     assert.deepEqual(sorted, notSorted, "This should return true");
   })
@@ -75,7 +78,28 @@ describe('#selectionSort', function(){
   var notSorted = [1,9,4,5,6,2,10,3,8,7];
   var sorted = [1,2,3,4,5,6,7,8,9,10];
   it('takes an unsorted array and sorts it', function(){
+    assert.notDeepEqual(sorted,notSorted, "This should return false")
     selectionSort(notSorted);
+    assert.deepEqual(sorted, notSorted, "This should return true");
+  })
+})
+
+describe('#insertionSort', function(){
+  var notSorted = [1,9,4,5,6,2,10,3,8,7];
+  var sorted = [1,2,3,4,5,6,7,8,9,10];
+  it('takes an unsorted array and sorts it', function(){
+    assert.notDeepEqual(sorted,notSorted, "This should return false")
+    insertionSort(notSorted);
+    assert.deepEqual(sorted, notSorted, "This should return true");
+  })
+})
+
+describe('#mergeSort', function(){
+  var notSorted = [1,9,4,5,6,2,10,3,8,7];
+  var sorted = [1,2,3,4,5,6,7,8,9,10];
+  it('takes an unsorted array and sorts it', function(){
+    assert.notDeepEqual(sorted,notSorted, "This should return false")
+    mergeSort(notSorted);
     assert.deepEqual(sorted, notSorted, "This should return true");
   })
 })
