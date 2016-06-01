4eb4b967c873e47223f4aed006a4f795e2d93f3b
Jason Tham <jason.tham@tempus.com>
2016-06-01 17:37:37 -0500

    added Binary Tree data structure to data module

--- data.js
+++ data.js
@@ -187,7 +187,7 @@ function List() {
   if (this.start === null) {
    this.start = List.makeNode();
    this.end = this.start;
-  } else { 
+  } else {
    this.end.next = List.makeNode();
    this.end = this.end.next;
   } ;
@@ -252,3 +252,66 @@ function List() {
   }
  };
 }
+
+module.exports.Tree = BinaryTree;
+
+function BinaryTree(){
+ this.Nodes = new Array();
+ this.level = 0;
+ this.node = 0;
+
+ this.setNode = function(value,level,node){
+  if (level === undefined) {
+  this.Nodes[this.btSMF(this.level, this.node)] = value;
+  }else {
+   this.Nodes[this.btSMF(level,node)] = value;
+  }
+ }
+
+ this.getNode = function(level, node){
+  if (level === undefined) {
+   return this.Nodes[this.btSMF(this.level,this.node)];
+  }else {
+   return this.Nodes[this.btSMF(level,node)];
+  }
+ }
+
+ this.root = function(value){
+  this.level = 0;
+  this.node = 0;
+  if (value !== undefined) {
+   this.Nodes[this.btSMF(this.level,this.node)] = value;
+  }
+  return this.Nodes[this.btSMF(this.level,this.node)];
+ }
+
+ this.leftChild = function(value){
+  this.level++;
+  this.node = this.node * 2;
+  if (value !== undefined) {
+   this.Nodes[this.btSMF(this.level,this.node)] = value;
+  }
+  return this.Nodes[this.btSMF(this.level,this.node)];
+ }
+
+ this.rightChild = function(value){
+  this.level++;
+  this.node = this.node * 2 + 1;
+  if (value !== undefined) {
+   this.Nodes[this.btSMF(this.level, this.node)] = value;
+  }
+  return this.Nodes[this.btSMF(this.level, this.node)];
+ }
+
+ this.parent = function(value){
+  this.level--;
+  this.node = this.node >> 1;
+  if (value !== undefined) {
+   this.Nodes[this.btSMF(this.level, this.node)] = value;
+  }
+  return this.Nodes[this.btSMF(this.level,this.node)];
+ }
+
+ this.btSMF = function(level, node){
+  return node + (1 << level) - 1;
+ }
